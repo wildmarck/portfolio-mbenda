7223380686df693f42c3d137add529f36c8b3ab2
wildmarck <160800188+wildmarck@users.noreply.github.com>
2026-01-16 20:23:22 +0100

    site-update

--- main.tf
+++ main.tf
@@ -9,26 +9,30 @@ terraform {
 
 provider "docker" {}
 
-# Définition d'une variable (Bonne pratique à montrer)
 variable "container_name" {
-  description = "Nom du conteneur pour le site web"
+  description = "Portfolio"
   type        = string
   default     = "site_web_etudiant_devops"
 }
 
-# Image Docker
 resource "docker_image" "nginx" {
   name         = "nginx:alpine"
   keep_locally = false
 }
 
-# Conteneur
 resource "docker_container" "site_web" {
   image = docker_image.nginx.image_id
   name  = var.container_name
-
+  
   ports {
     internal = 80
     external = 8080
+  }
+
+  # --- C'EST ICI QUE LA MAGIE OPÈRE ---
+  # On dit au conteneur : "Remplace ton dossier html par mon dossier actuel"
+  volumes {
+    host_path      = abspath(path.cwd)        # Chemin absolu de ton dossier projet
+    container_path = "/usr/share/nginx/html"  # Dossier cible dans Nginx
   }
 }
